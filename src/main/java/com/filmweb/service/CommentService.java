e49c70dd4c0bd739424434fabafe9fae9bbb161a
Vo Truong Lam <votruonglam2109@gmail.com>
2024-04-14 17:28:41 +0700

    add animation when a user likes video, show more comments in video

--- src/main/java/com/filmweb/service/CommentService.java
+++ src/main/java/com/filmweb/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.filmweb.service;
 
+import com.filmweb.api.CommentListResp;
 import com.filmweb.dto.CommentDto;
 import com.filmweb.entity.Comment;
 import com.filmweb.entity.Video;
@@ -9,11 +10,17 @@ import java.util.List;
 public interface CommentService {
     List<Comment> findByUser(String username);
 
-    List<CommentDto> findByVideoId(Long videoId);
+    List<CommentDto> findByVideoId(Long videoId, int page, int limit);
 
     Comment findByUserIdAndVideoId(Long userId, Long videoId);
 
     Comment findByVideoIdGetUser(Long videoId);
 
     Comment create(Long userId, Long videoId, String content);
+
+    long count();
+
+    CommentListResp loadMoreComments(String href, int page, int limit);
+
+    int getLastPageByVideoHref(String href, int limit);
 }
