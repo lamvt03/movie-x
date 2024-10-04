467a095f8349108293cab94dae558db88d228e7f
Lam Vo <votruonglam2109@gmail.com>
2024-10-04 21:21:29 +0700

    :construction:

--- src/main/java/com/filmweb/config/MailConfigurationProperties.java
+++ src/main/java/com/filmweb/config/MailConfigurationProperties.java
@@ -1,10 +1,8 @@
 package com.filmweb.config;
 
 import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
 import lombok.Getter;
 import org.eclipse.microprofile.config.inject.ConfigProperties;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ConfigProperties(prefix = "mail")
 @Dependent
