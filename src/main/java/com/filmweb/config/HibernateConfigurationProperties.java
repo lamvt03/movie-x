6581cb0f773ff4b52fc65d6d96059263f236edf4
Lam Vo <votruonglam2109@gmail.com>
2024-10-16 01:07:23 +0700

    :zap: Hibernate configuration properties

---
+++ src/main/java/com/filmweb/config/HibernateConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.filmweb.config;
+
+import jakarta.enterprise.context.Dependent;
+import lombok.Getter;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+
+@ConfigProperties(prefix = "hibernate")
+@Dependent
+@Getter
+public class HibernateConfigurationProperties {
+    
+    String driverClass;
+    String url;
+    String username;
+    String password;
+    String dialect;
+    String ddlAuto;
+}
